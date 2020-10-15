592c0c1620e71f52502571fc3af1bbbcdf68bf35
Inspiron <nityanandasamanta96@gmail.com>
2020-10-15 22:49:35 +0530

    appbar has been removed from the most front page.Also login activity has been created.

--- app/src/main/java/munik/androidprojects/tradeapp/MainActivity.kt
+++ app/src/main/java/munik/androidprojects/tradeapp/MainActivity.kt
@@ -1,11 +1,28 @@
 package munik.androidprojects.tradeapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.frontPage)
+        setContentView(R.layout.frontpage)
+
+        //added timer so that ,after the 5 seconds of the apps opening it will go to the login page
+        val timer:Timer= Timer()
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                //this method will take us to the login Activity
+                goTologinPage()
+            }
+        }, 3500)
+    }
+    private fun goTologinPage(){
+
+        val goTologinPage = Intent(this, LoginPage::class.java)
+        startActivity(goTologinPage)
+        finish()
     }
 }
