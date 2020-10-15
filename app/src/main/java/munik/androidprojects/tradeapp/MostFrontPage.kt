d0715a1117da46c26355c85e9da16917864352a0
Inspiron <nityanandasamanta96@gmail.com>
2020-10-15 23:06:31 +0530

    changed the name of the mainactivity to MostFrontPage.kt

--- app/src/main/java/munik/androidprojects/tradeapp/MostFrontPage.kt
+++ app/src/main/java/munik/androidprojects/tradeapp/MostFrontPage.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MostFrontPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.frontpage)
