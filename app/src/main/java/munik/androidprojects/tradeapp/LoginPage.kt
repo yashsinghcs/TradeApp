2f2c45e9be4229e0f7d191c313231a272c46ceeb
balaram714 <nityanandasamanta96@gmail.com>
2020-10-29 00:07:30 +0530

    adde signUp page in the project

--- app/src/main/java/munik/androidprojects/tradeapp/LoginPage.kt
+++ app/src/main/java/munik/androidprojects/tradeapp/LoginPage.kt
@@ -1,11 +1,21 @@
 package munik.androidprojects.tradeapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginPage : AppCompatActivity() {
+    private lateinit var signup_button_LoginPage:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
+
+        //initialising the View objects.
+        signup_button_LoginPage = findViewById(R.id.signup)
+
+        signup_button_LoginPage.setOnClickListener {
+            startActivity(Intent(this,SignupPage::class.java))
+        }
     }
 }
