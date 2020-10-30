d6d6a6456101c4bdd0fe9783b9e88018ee35bca3
balaram714 <nityanandasamanta96@gmail.com>
2020-10-31 00:48:10 +0530

    added forget password activity to the project

--- app/src/main/java/munik/androidprojects/tradeapp/LoginPage.kt
+++ app/src/main/java/munik/androidprojects/tradeapp/LoginPage.kt
@@ -7,15 +7,21 @@ import android.widget.TextView
 
 class LoginPage : AppCompatActivity() {
     private lateinit var signup_button_LoginPage:TextView
+    private lateinit var forgetpassword:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
 
         //initialising the View objects.
         signup_button_LoginPage = findViewById(R.id.signup)
+        forgetpassword = findViewById(R.id.forgetpassword_textview)
 
         signup_button_LoginPage.setOnClickListener {
             startActivity(Intent(this,SignupPage::class.java))
+        }
+
+        forgetpassword.setOnClickListener {
+            startActivity( Intent(this,ForgetPassword::class.java))
         }
     }
 }
