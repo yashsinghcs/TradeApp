54df835a928642572435b456de7db8d02fe516d6
balaram714 <nityanandasamanta96@gmail.com>
2020-11-07 22:31:51 +0530

    added activity which will be visible after the login page

---
+++ app/src/main/java/munik/androidprojects/tradeapp/ItemListPageAfterLoginUser.kt
@@ -0,0 +1,11 @@
+package munik.androidprojects.tradeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ItemListPageAfterLoginUser : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_list_page_after_login_user)
+    }
+}
--- app/src/main/java/munik/androidprojects/tradeapp/LoginPage.kt
+++ app/src/main/java/munik/androidprojects/tradeapp/LoginPage.kt
@@ -3,11 +3,13 @@ package munik.androidprojects.tradeapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class LoginPage : AppCompatActivity() {
     private lateinit var signup_button_LoginPage:TextView
     private lateinit var forgetpassword:TextView
+    private lateinit var loginbutton : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
@@ -15,6 +17,7 @@ class LoginPage : AppCompatActivity() {
         //initialising the View objects.
         signup_button_LoginPage = findViewById(R.id.signup)
         forgetpassword = findViewById(R.id.forgetpassword_textview)
+        loginbutton = findViewById(R.id.login)
 
         signup_button_LoginPage.setOnClickListener {
             startActivity(Intent(this,SignupPage::class.java))
@@ -22,6 +25,12 @@ class LoginPage : AppCompatActivity() {
 
         forgetpassword.setOnClickListener {
             startActivity( Intent(this,ForgetPassword::class.java))
+        }
+
+        loginbutton.setOnClickListener {
+
+            startActivity( Intent(this,ItemListPageAfterLoginUser::class.java))
+
         }
     }
 }
