46b2eeff90d4395289d1ca45fe2b934fac27b1db
balaram714 <nityanandasamanta96@gmail.com>
2020-11-24 00:26:56 +0530

    Login done successfully

--- app/src/main/java/munik/androidprojects/tradeapp/LoginPage.kt
+++ app/src/main/java/munik/androidprojects/tradeapp/LoginPage.kt
@@ -4,12 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class LoginPage : AppCompatActivity() {
     private lateinit var signup_button_LoginPage:TextView
     private lateinit var forgetpassword:TextView
     private lateinit var loginbutton : Button
+    private lateinit var Auth: FirebaseAuth
+    private lateinit var username: EditText
+    private lateinit var password: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
@@ -18,7 +25,9 @@ class LoginPage : AppCompatActivity() {
         signup_button_LoginPage = findViewById(R.id.signup)
         forgetpassword = findViewById(R.id.forgetpassword_textview)
         loginbutton = findViewById(R.id.login)
-
+        username=findViewById(R.id.username)
+        password=findViewById(R.id.password)
+        Auth=FirebaseAuth.getInstance()
         signup_button_LoginPage.setOnClickListener {
             startActivity(Intent(this,SignupPage::class.java))
         }
@@ -28,9 +37,54 @@ class LoginPage : AppCompatActivity() {
         }
 
         loginbutton.setOnClickListener {
-
-            startActivity( Intent(this,ItemListPageAfterLoginUser::class.java))
+            doLogin()
+            //startActivity( Intent(this,ItemListPageAfterLoginUser::class.java))
 
         }
+    }
+
+    private fun doLogin(){
+        if (username.text.toString().isEmpty()) {
+            username.error = "please enter username"
+            username.requestFocus()
+            return
+        }
+        if (password.text.toString().isEmpty()) {
+            password.error = "please enter password"
+            password.requestFocus()
+            return
+        }
+
+        Auth.signInWithEmailAndPassword(username.text.toString(),password.text.toString()).addOnCompleteListener{
+                task ->
+            if(task.isSuccessful){
+                val user: FirebaseUser?=Auth.currentUser
+                updateUI(user)
+            }
+
+            else{
+
+                updateUI(null)
+            }
+        }
+    }
+
+    private  fun updateUI(currentuser:FirebaseUser?){
+        if(currentuser!=null){
+            Toast.makeText(baseContext,"Login sucsessful", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this,ItemListPageAfterLoginUser::class.java))
+            finish()
+        }
+        else{
+            Toast.makeText(baseContext,"Login Failed", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        val user:FirebaseUser?=Auth.currentUser
+        updateUI(user)
+
     }
 }
